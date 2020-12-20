cab1b9b78729b00dc30553d0358b44859b1ab476
Ashutosh kumar gautam <65886012+Ashutoshkumargautam@users.noreply.github.com>
2020-12-21 01:25:14 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,14 @@
-
+#!/use/bin/env python
+import OpenCV
+import matplo
+import Numpy
+pip install SciPy
+Pip install Scikit-learn
+Pip install Theano
+Pip install TensorFlow
+pip install Keras
+pip install PyTorch
+pip install Pandas
+pip install pyttsx
+pip install NLTK
+pip install Luminoth
